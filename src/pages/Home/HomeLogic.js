e16323df13115528c1ec51ded9d644d87ef051a0
mateotiedra <mateo@tiedra.com>
2022-07-05 15:26:06 +0200

    remove useless from home logic

--- src/pages/Home/HomeLogic.js
+++ src/pages/Home/HomeLogic.js
@@ -3,33 +3,18 @@ import { useState } from 'react';
 import PageLogicHelper from '../../helpers/PageLogicHelper';
 
 const HomeLogic = () => {
-  const { pageStatus, setPageStatus, useLoadPage, axios, API_ORIGIN } =
-    PageLogicHelper();
-
-  const [linksDialogOpen, setLinksDialogOpen] = useState(false);
-
-  const handleCloseLinksDialog = () => setLinksDialogOpen(false);
-  const handleOpenLinksDialog = () => setLinksDialogOpen(true);
-
-  const [newsList, setNewsList] = useState();
-
-  useLoadPage(() => {
-    axios
-      .get(API_ORIGIN + '/news/all', { params: { limit: 3 } })
-      .then(({ data }) => {
-        setNewsList(data);
-        setPageStatus('active');
-      })
-      .catch((err) => {});
-  }, {});
-
-  return {
-    linksDialogOpen,
-    handleCloseLinksDialog,
-    handleOpenLinksDialog,
+  const {
     pageStatus,
-    newsList,
-  };
+    setPageStatus,
+    useLoadPage,
+    axios,
+    API_ORIGIN,
+    setErrorCode,
+  } = PageLogicHelper();
+
+  useLoadPage(() => {}, {});
+
+  return {};
 };
 
 export default HomeLogic;
