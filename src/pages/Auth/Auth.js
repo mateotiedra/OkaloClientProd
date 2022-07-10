3120d3943013e358bec9484536a6b1789eb9084a
mateotiedra <mateo@tiedra.com>
2022-07-09 23:33:47 -0400

    simplify html

--- src/pages/Auth/Auth.js
+++ src/pages/Auth/Auth.js
@@ -36,89 +36,79 @@ export default function Auth({ startingMode }) {
     <>
       <Navbar coverPage empty />
       <SectionContainer
-        component='main'
         maxWidth='sm'
         sx={{
           height: '100vh',
           display: 'flex',
           flexDirection: 'column',
           justifyContent: 'center',
+          alignItems: 'center',
+          pt: 8,
         }}
       >
+        <Avatar sx={{ m: 1 }}>
+          <HiUser />
+        </Avatar>
+        <Typography component='h1' variant='h5'>
+          {loginMode ? 'Connexion' : 'Inscription'}
+        </Typography>
         <Box
+          component='form'
+          noValidate
+          onSubmit={onSubmit}
           sx={{
-            marginTop: 8,
+            mt: 2,
             display: 'flex',
             flexDirection: 'column',
-            alignItems: 'center',
+            gap: 2,
+            width: '100%',
           }}
         >
-          <Avatar sx={{ m: 1 }}>
-            <HiUser />
-          </Avatar>
-          <Typography component='h1' variant='h5'>
-            {loginMode ? 'Connexion' : 'Inscription'}
-          </Typography>
-          <Box
-            component='form'
-            noValidate
-            onSubmit={onSubmit}
-            sx={{
-              mt: 2,
-              display: 'flex',
-              flexDirection: 'column',
-              gap: 2,
-              width: '100%',
-            }}
+          <TextField
+            id='email'
+            label='Adresse email'
+            autoComplete='email'
+            autoFocus
+            helperText={errors['email'] && errors['email'].message}
+            error={errors['email'] !== undefined}
+            {...register('email', {
+              required: true,
+              pattern: /^[\w]+@([\w-]+\.)+[\w-]{2,4}$/g,
+            })}
+          />
+          {!loginMode && <RegisterFields register={register} errors={errors} />}
+          <PasswordField
+            required
+            id='password'
+            label='Mot de passe'
+            errors={errors}
+            registration={register('password', {
+              required: true,
+            })}
+          />
+          <LoadingButton
+            variant='contained'
+            type='submit'
+            fullWidth
+            sx={{ mt: 3, mb: 2 }}
+            loading={pageStatus === 'sending'}
           >
-            <TextField
-              id='email'
-              label='Adresse email'
-              autoComplete='email'
-              autoFocus
-              helperText={errors['email'] && errors['email'].message}
-              error={errors['email'] !== undefined}
-              {...register('email', {
-                required: true,
-                pattern: /^[\w]+@([\w-]+\.)+[\w-]{2,4}$/g,
-              })}
-            />
-            {!loginMode && (
-              <RegisterFields register={register} errors={errors} />
-            )}
-            <PasswordField
-              required
-              id='password'
-              label='Mot de passe'
-              errors={errors}
-              registration={register('password', {
-                required: true,
-              })}
-            />
-            <LoadingButton
-              variant='contained'
-              type='submit'
-              fullWidth
-              sx={{ mt: 3, mb: 2 }}
-              loading={pageStatus === 'sending'}
-            >
-              <Typography variant='body1'>
-                {loginMode ? 'Se connecter' : "S'inscrire"}
-              </Typography>
-            </LoadingButton>
-            <Grid container>
-              <Grid item xs>
-                <Link variant='body2' onClick={switchLoginMode}>
-                  {loginMode ? "S'inscrire" : 'Se connecter'}
-                </Link>
-              </Grid>
-              <Grid item md>
-                <Link href='#' variant='body2'>
-                  {loginMode && false && 'Mot de passe oublié ?'}
-                </Link>
-              </Grid>
+            <Typography variant='body1'>
+              {loginMode ? 'Se connecter' : "S'inscrire"}
+            </Typography>
+          </LoadingButton>
+          <Grid container>
+            <Grid item xs>
+              <Link variant='body2' onClick={switchLoginMode}>
+                {loginMode ? "S'inscrire" : 'Se connecter'}
+              </Link>
             </Grid>
-          </Box>
+            <Grid item md>
+              <Link href='#' variant='body2'>
+                {loginMode && false && 'Mot de passe oublié ?'}
+              </Link>
+            </Grid>
+          </Grid>
         </Box>
         <Footer push={false} />
       </SectionContainer>
