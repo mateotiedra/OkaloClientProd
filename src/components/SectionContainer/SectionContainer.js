8362eb6f91ff41016f16e884772bac3e01898062
mateotiedra <mateo@tiedra.com>
2022-07-29 16:07:36 +0200

    add centered prop and redo structure of it

--- src/components/SectionContainer/SectionContainer.js
+++ src/components/SectionContainer/SectionContainer.js
@@ -1,20 +1,29 @@
 import { Container } from '@mui/material';
 
-function SectionContainer(props) {
+function SectionContainer({
+  id,
+  ref,
+  maxWidth,
+  sx,
+  children,
+  centered,
+  ...props
+}) {
   return (
     <Container
-      id={props.id}
-      ref={props.ref}
-      maxWidth={props.maxWidth || 'lg'}
+      id={id}
+      ref={ref}
+      maxWidth={maxWidth || 'lg'}
       sx={{
-        px: { xs: 4, sm: 5, md: 10, lg: 10, ...props.sx },
+        px: { xs: 4, sm: 5, md: 10, lg: 10, ...sx },
         display: 'flex',
         flexDirection: 'column',
-        alignItems: 'center',
-        ...props.sx,
+        alignItems: centered && 'center',
+        ...sx,
       }}
+      {...props}
     >
-      {props.children}
+      {children}
     </Container>
   );
 }
