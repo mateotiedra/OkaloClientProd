ab3f046d7e9b15dafffd2870d9c737c435a1911e
mateotiedra <mateo@tiedra.com>
2022-07-30 12:21:31 +0200

    add section container

--- src/pages/NewBid/NewBid.js
+++ src/pages/NewBid/NewBid.js
@@ -1,9 +1,16 @@
 import React from 'react';
 
+import { Box } from '@mui/material';
+import { HiBookOpen } from 'react-icons/hi';
+
 import Navbar from '../../components/Navbar/Navbar';
-import SectionContainer from '../../components/SectionContainer/SectionContainer';
+import FormFields from '../../components/FormFields/FormFields';
 import Loading from '../Loading/Loading';
 import NewBidLogic from './NewBidLogic';
+import Footer from '../../components/Footer/Footer';
+import IconTitle from '../../components/IconTitle/IconTitle';
+import SectionContainer from '../../components/SectionContainer/SectionContainer';
+import SectionDivider from '../../components/SectionDivider/SectionDivider';
 
 function NewBid() {
   const { pageStatus } = NewBidLogic();
@@ -13,13 +20,21 @@ function NewBid() {
   return (
     <>
       <Navbar />
-      <SectionContainer
-        sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-        }}
-      ></SectionContainer>
+      <SectionContainer>
+        <SectionDivider />
+        <IconTitle icon={<HiBookOpen />}>Mettre un livre en vente</IconTitle>
+      </SectionContainer>
+      {/* <FormFields
+        page
+        centered
+        title={'Nouvelle annonce'}
+        avatarIcon={<HiBookOpen />}
+        sending={pageStatus === 'sending'}
+        buttonText={'Suivant'}
+        fields={[]}
+      >
+        <Footer />
+      </FormFields> */}
     </>
   );
 }
